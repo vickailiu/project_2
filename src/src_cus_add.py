edbc732e212e530319368cf6e1462a78e177a780
kai.liu <kai.lk.liu@deltaww.com>
2020-11-30 19:17:04 +0800

    submodule

--- src/src_cus_add.py
+++ src/src_cus_add.py
@@ -1,4 +1,13 @@
+from .shared_assets.sub_add import sub_add
+
+
 def src_cus_add(a, b):
     print("")
-    print("called_from src_cus_add")
+    print("project 2called_from src_cus_add")
     return a + b
+
+
+def src_call_sub_add(a, b):
+    print("")
+    print("project 2 called from src_call_sub_add")
+    return sub_add(a, b)
